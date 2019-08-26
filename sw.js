630a3c44653a672ceb19873b70591c7659c3217e
darkylin <darkylin1989@gmail.com>
2019-08-26 18:08:14 +0800

    test

---
+++ sw.js
@@ -0,0 +1,50 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/styles/main.css',
+    '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+self.addEventListener('fetch', function(event) {
+    console.log('sw fetch', event.request.url)
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        console.log('hit cache')
+                        return response;
+                    }
+                    console.log('fetch')
+                    return fetch(event.request).then(
+                        function(response) {
+                            // Check if we received a valid response
+                            if(!response || response.status !== 200 || response.type !== 'basic') {
+                                return response;
+                            }
+
+                            console.log('start cache')
+                            var responseToCache = response.clone();
+
+                            caches.open(CACHE_NAME)
+                                .then(function(cache) {
+                                    cache.put(event.request, responseToCache);
+                                });
+
+                            return response;
+                        }
+                    );
+                }
+            )
+    );
+});
